561bafec6fdc20fcd0f48d3841a4651c3795f9ed
ivanZhuravlov <ivanzhuravlovofficial00@gmail.com>
2021-09-16 16:49:45 +0300

    Added news request

--- app/controllers/news.controller.js
+++ app/controllers/news.controller.js
@@ -3,15 +3,20 @@
  * @module controllers/info
  */
 
-
+const axios = require('axios')
+const API_KEY = process.env.API_KEY
 /**
  * @param req
  * @param res
  */
-exports.newsList = (req,res) => {
+exports.newsList = async (req,res) => {
+
+    const result = await axios.get(`https://newsapi.org/v2/everything?q=Apple&from=2021-09-15&sortBy=popularity&apiKey=${API_KEY}`)
+    const data = result.data.articles
+
     return res.json({
         status: 200,
         message: 'Success',
-        data: [],
+        data,
     })
 }
